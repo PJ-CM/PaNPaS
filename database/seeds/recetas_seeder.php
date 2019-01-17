edc1d91cc32d65962c26b14f1f3508682118921c
Enekovaz4 <Enekovaz4@gmail.com>
2019-01-17 14:18:39 +0100

    fecha en modal de landing

--- database/seeds/recetas_seeder.php
+++ database/seeds/recetas_seeder.php
@@ -15,11 +15,12 @@ class recetas_seeder extends Seeder
         for ($i = 0; $i < 30; $i++){
 	         DB::table('recetas')->insert([
 		            'titulo' => 'titulo'.$i,
-		            'descripcion' => 'correo'.$i,
+		            'descripcion' => $faker->realText($faker->numberBetween(200,255)),
 		            'imagen' => $faker->imageUrl($width = 640, $height = 480),
 		            'elaboracion' => 'elaboracion'.$i,
 		            'user_id' => random_int(1, 9),
-                    'votos' => random_int(0, 999)
+                    'votos' => random_int(0, 999),
+                    'created_at' => $faker->date($format = 'Y-m-d', $max = 'now')
 	        ]);
 	    }
     }
