54dc1c387686ef28f74d33d0af2c6726ae1b184f
PJCM-ZubiMan <pcrespoma18dw@ikzubirimanteo.com>
2019-01-18 11:50:38 +0100

    Redirigir a página de inicio el usuario que acceda a sección restringida sin autenticarse

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,12 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            //Ruta a la que redirigir el usuario si no está autenticado
+            //y accede a un lugar restringido
+            //  >> por defecto
+            ////return route('login');
+            //  >> indicando que se redirija a una determinada vista
+            return route('index');
         }
     }
 }
